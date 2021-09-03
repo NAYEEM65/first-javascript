10fe166b69e32852c2dc4383021a91cea14bea1f
NAYEEM65 <70934241+NAYEEM65@users.noreply.github.com>
2021-09-04 01:02:34 +0600

    files upload

---
+++ app.js
@@ -0,0 +1,105 @@
+// const btnOff = document.getElementById('btnOff');
+// const btnOn = document.getElementById('btnOn');
+// const bulbOff = document.getElementById('bulbOff');
+// const bulbOn = document.getElementById('bulbOn');
+
+// btnOff.addEventListener('click', function(){
+//     bulbOn.style.display='none';
+//     bulbOff.style.display='block';
+// });
+
+// btnOn.addEventListener('click', function(){
+//     bulbOn.style.display='block';
+//     bulbOff.style.display='none';
+// })
+
+// var a = 2;
+// var b = 8;
+// a -= b; //a = a-b
+// console.log(a);
+
+// const a = 6;
+// const b = 9;
+// const sum = a+b;
+// console.log(sum);
+
+// let a = 16;
+// let b = 9;
+// let sum = a+b;
+// console.log(sum);
+
+// var r = 5;
+// var pi = 3.1416;
+// var area = pi*r**6;
+// console.log('$'+ area);
+
+// var a = 3;
+// var b = a;
+// console.log(b);
+// if(a===b){
+//     console.log('a and b are equal');
+// }
+// else{
+//     console.log('a and b are not equal');
+// }
+
+// var c = 37.5;
+// var f = (9*c+160)/5;
+// console.log(f);
+
+
+// var a = 4;
+// if(a%2 == 0){
+//     console.log('the number is even')
+// }
+// else{
+//     console.log('the number is odd');
+// }
+
+//  var a = 6;
+//  var b = 4;
+//  if(a%2 == 0 && b%2 == 0){
+//      console.log('both are even number');
+//  }
+//  else{
+//      console.log('both are odd number');
+//  }
+
+//  var a = 5;
+//  var b = 10;
+//  if(a%2 == 0 || b%2 == 0){
+//      console.log('anyone is even number');
+//  }
+//  else{
+//      console.log('both are odd number');
+//  }
+
+//  var a = 5; 
+//  var b = 5;
+//  if(a != b){
+//      console.log(' a and b are not equal');
+//  }
+//  else{
+//      console.log(' a and b are equal');
+//  }
+
+ var a = 5;
+ var b = "Rabbi";
+ var car;
+ console.log(car)
+
+ var details = {firstName: "Golam", lastName: "Rabbi" }
+ console.log(typeof(a));
+ console.log(a==b.length);
+//  console.log(b.length);
+//  console.log(typeof(details));
+//  console.log(details.firstName +' '+ details.lastName);
+
+//  if(b.length%2==0){
+//      console.log('even')
+//  }
+//  else{
+//      console.log('odd')
+//  }
+
+
